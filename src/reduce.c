d85d7db6383c0b60a4aae874dc177ce91f8052d5
Daniel <danielelacer08@gmail.com>
2025-08-14 09:43:47 +0100

    Make reduction work on rows of input, add wrapper.

--- src/reduce.c
+++ src/reduce.c
@@ -1,4 +1,5 @@
 #include "reduce.h"
+#include "cl_utils.h"
 #include <stdio.h>
 
 /* Format strings:
@@ -9,37 +10,43 @@
  */
 const char *_reduce_1step_fmt = RAW (__kernel void entry (
     const int a1, const int a2, const int a3, __global % s * A) {
-  int idx = get_global_id (0);
-  int local_idx = get_local_id (0);
-  int group_idx = get_group_id (0);
-  int total = a1 * a2 * a3;
-  const int tile_size = % d;
-  __local % s A_tile[tile_size];
+  int row = get_global_id (1);
+  int col = get_global_id (0);
 
-  if (idx < total)
+  int local_row = get_local_id (1);
+  int local_col = get_local_id (0);
+
+  int group_row = get_group_id (1);
+  int group_col = get_group_id (0);
+  const int tile_size = % d;
+  __local % s A_tile[tile_size][tile_size];
+
+  if (col < a1)
     {
-      A_tile[local_idx] = A[idx];
+      A_tile[local_row][local_col] = A[col + a1 * row];
       barrier (CLK_LOCAL_MEM_FENCE);
 
-      for (int offset = tile_size / 2; offset > 0; offset >>= 1)
+      for (int offset = get_local_size (0) / 2; offset > 0; offset >>= 1)
         {
-          if (local_idx < offset && (idx + offset) < total)
+          if (local_col < offset && (col + offset) < a1)
             {
-              A_tile[local_idx]
-                  = A_tile[local_idx] % s A_tile[local_idx + offset];
+              A_tile[local_row][local_col]
+                  = A_tile[local_row][local_col]
+                    % s A_tile[local_row][local_col + offset];
             }
+
           barrier (CLK_LOCAL_MEM_FENCE);
         }
 
-      if (local_idx == 0)
+      if (local_col == 0 && row < a2)
         {
-          A[group_idx] = A_tile[0];
+          A[group_col + a1 * row] = A_tile[local_row][0];
         }
     }
 });
 
 char *
-_get_reduce_1step (const char *dtype, const char *op1)
+get_reduce_1step (const char *dtype, const char *op1)
 {
   char *kernel = NULL;
 
@@ -53,24 +60,39 @@ _get_reduce_1step (const char *dtype, const char *op1)
   return kernel;
 }
 
-cl_kernel
-reduce_1step (const char *op1, array A)
+void
+reduce (const char *op1, array A, cl_event *event)
 {
-  cl_kernel kernel = NULL;
-
   cl_int err;
   const char *dtype = TYPE_STR_FROM_ENUM (A.type);
-  char *src = _get_reduce_1step (dtype, op1);
+  char *src = get_reduce_1step (dtype, op1);
   cl_program program = CHECK_CL (
       clCreateProgramWithSource (_context, 1, (const char **)&src, NULL, &err),
       err);
   TRY_BUILD_PROGRAM (program);
   free (src);
 
-  kernel = CHECK_CL (clCreateKernel (program, "entry", &err), err);
+  cl_kernel kernel = CHECK_CL (clCreateKernel (program, "entry", &err), err);
   clReleaseProgram (program);
 
   SET_KERNEL_ARGS (kernel, A);
 
-  return kernel;
+  size_t local_size[] = { _tile_size, _tile_size, _tile_size };
+  size_t global_size[]
+      = { LOWEST_MULTIPLE_OF_TILE (A.dim1), LOWEST_MULTIPLE_OF_TILE (A.dim2),
+          LOWEST_MULTIPLE_OF_TILE (A.dim3) };
+
+  for (; global_size[0] > _tile_size;
+       global_size[0] = (global_size[0] + _tile_size - 1) / _tile_size)
+    {
+      CHECK_CL (clEnqueueNDRangeKernel (_queue, kernel, ARRAY_NUM_DIMS (A),
+                                        NULL, global_size, local_size, 0, NULL,
+                                        event));
+    }
+  if (global_size[0] > 1)
+    {
+      CHECK_CL (clEnqueueNDRangeKernel (_queue, kernel, ARRAY_NUM_DIMS (A),
+                                        NULL, global_size, NULL, 0, NULL,
+                                        event));
+    }
 }
--- src/reduce.h
+++ src/reduce.h
@@ -4,7 +4,11 @@
 #include "cl_utils.h"
 
 extern const char *_reduce_1step_fmt;
-char *_get_reduce_1step (const char *dtype, const char *op1);
-cl_kernel reduce_1step (const char *op1, array A);
+char *get_reduce_1step (const char *dtype, const char *op1);
+void reduce (const char *op1, array A, cl_event *event);
+#define _REDUCE_ONE(op1, A) reduce (op1, A, NULL);
+#define _REDUCE_TWO(op1, A, event) reduce (op1, A, event)
+#define REDUCE(...)                                                           \
+  _GETM_THREE (__VA_ARGS__, _REDUCE_TWO, _REDUCE_ONE) (__VA_ARGS__)
 
 #endif // REDUCE_H_
