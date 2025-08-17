4c8e78e782dc1acdf4d9faacfa72d0bbc071a421
Daniel <danielelacer08@gmail.com>
2025-08-17 07:13:04 +0100

    Change op1 usage for map and outer product.

--- src/map.c
+++ src/map.c
@@ -3,26 +3,32 @@
 #include <stdio.h>
 
 /* Format strings:
- * 1. A type
- * 2. B type
- * 3. OP1
+ ** 1. A type
+ ** 2. B type
+ ** 3. A type
+ ** 4. B type
+ ** 5. OP1
  */
-
 const char *_map_fmt = RAW (__kernel void entry (
     const int a1, const int a2, const int a3, __global const % s * A,
     const int b1, const int b2, const int b3, __global % s * B) {
-  int idx = get_global_id (0);
+  int global_id = get_global_id (0);
+  int local_id = get_local_id (0);
+  int group_id = get_group_id (0);
+
   int total = a1 * a2 * a3;
-  if (idx < total)
+  if (global_id < total)
     {
-      B[idx] = % s (A[idx]);
+      % s a = A[global_id];
+      % s b = B[global_id];
+      B[global_id] = % s;
     }
 });
 
 char *
-get_map (const char *dtype, const char *op1)
+get_map (const char *atype, const char *btype, const char *op1)
 {
-  int size = snprintf (NULL, 0, _map_fmt, dtype, dtype, op1);
+  int size = snprintf (NULL, 0, _map_fmt, atype, btype, atype, btype, op1);
 
   char *kernel = malloc (size + 1);
   if (!kernel)
@@ -30,7 +36,8 @@ get_map (const char *dtype, const char *op1)
       handle_error ("Failed to allocate memory for kernel string");
     }
 
-  int count = snprintf (kernel, size + 1, _map_fmt, dtype, dtype, op1);
+  int count
+      = snprintf (kernel, size + 1, _map_fmt, atype, btype, atype, btype, op1);
   if (count == -1)
     {
       handle_error ("Failed to print to kernel string");
@@ -43,8 +50,8 @@ unsigned long long
 map (const char *op1, array A, array B, cl_event *event)
 {
   cl_event _event;
-  const char *dtype = TYPE_STR_FROM_ENUM (B.type);
-  char *src = get_map (dtype, op1);
+  char *src = get_map (TYPE_STR_FROM_ENUM (A.type),
+                       TYPE_STR_FROM_ENUM (B.type), op1);
   cl_kernel kernel = TRY_COMPILE_KERNEL (src);
   free (src);
   SET_KERNEL_ARGS (kernel, A, B);
@@ -53,8 +60,8 @@ map (const char *op1, array A, array B, cl_event *event)
   size_t global_size[]
       = { LOWEST_MULTIPLE_OF_TILE (B.dim1), LOWEST_MULTIPLE_OF_TILE (B.dim2),
           LOWEST_MULTIPLE_OF_TILE (B.dim3) };
-  CHECK_CL (clEnqueueNDRangeKernel (_queue, kernel, ARRAY_NUM_DIMS (B), NULL,
-                                    global_size, local_size, 0, NULL,
+  CHECK_CL (clEnqueueNDRangeKernel (_queue, kernel, 1, NULL, global_size,
+                                    local_size, 0, NULL,
                                     event ? event : &_event));
 
   unsigned long long time = 0;
--- src/outer_product.c
+++ src/outer_product.c
@@ -6,24 +6,47 @@
 ** 1. A type
 ** 2. B type
 ** 3. C type
-** 4. OP1
+** 4. TILE_SIZE
+** 5. A_tile type
+** 6. B_tile type
+** 7. A type
+** 8. B type
+** 9. OP1
 */
 const char *_outer_product_fmt = RAW (__kernel void entry (
-    const int a1, const int a2, const int a3, __global const % s * A,
-    const int b1, const int b2, const int b3, __global const % s * B,
+    const int a1, const int a2, const int a3, __global const % s * _A,
+    const int b1, const int b2, const int b3, __global const % s * _B,
     const int c1, const int c2, const int c3, __global % s * C) {
-  int row = get_global_id (1);
-  int col = get_global_id (0);
-  if (col < c1 && row < c2)
+  int global_row = get_global_id (1);
+  int global_col = get_global_id (0);
+
+  int local_row = get_local_id (1);
+  int local_col = get_local_id (0);
+  int row = local_row;
+  int col = local_col;
+
+  const int tile_size = % d;
+  __local % s A[tile_size];
+  __local % s B[tile_size];
+
+  A[local_row] = _A[row];
+  B[local_col] = _B[col];
+  barrier (CLK_LOCAL_MEM_FENCE);
+
+  if (global_col < c1 && global_row < c2)
     {
-      C[row * c1 + col] = A[row] % s B[col];
+      % s a = A[local_row];
+      % s b = B[local_col];
+      C[row * c1 + col] = % s;
     }
 });
 
 char *
-get_outer_product (const char *dtype, const char *op1)
+get_outer_product (const char *atype, const char *btype, const char *ctype,
+                   const char *op1)
 {
-  int size = snprintf (NULL, 0, _outer_product_fmt, dtype, dtype, dtype, op1);
+  int size = snprintf (NULL, 0, _outer_product_fmt, atype, btype, ctype,
+                       _tile_size, atype, btype, atype, btype, op1);
 
   char *kernel = malloc (size + 1);
   if (!kernel)
@@ -31,8 +54,8 @@ get_outer_product (const char *dtype, const char *op1)
       handle_error ("Failed to allocate memory for kernel string");
     }
 
-  int count = snprintf (kernel, size + 1, _outer_product_fmt, dtype, dtype,
-                        dtype, op1);
+  int count = snprintf (kernel, size + 1, _outer_product_fmt, atype, btype,
+                        ctype, _tile_size, atype, btype, atype, btype, op1);
   if (count == -1)
     {
       handle_error ("Failed to print to kernel string");
@@ -45,8 +68,9 @@ unsigned long long
 outer_product (const char *op1, array A, array B, array C, cl_event *event)
 {
   cl_event _event;
-  const char *dtype = TYPE_STR_FROM_ENUM (C.type);
-  char *src = get_outer_product (dtype, op1);
+  char *src = get_outer_product (TYPE_STR_FROM_ENUM (A.type),
+                                 TYPE_STR_FROM_ENUM (B.type),
+                                 TYPE_STR_FROM_ENUM (C.type), op1);
   cl_kernel kernel = TRY_COMPILE_KERNEL (src);
   free (src);
   SET_KERNEL_ARGS (kernel, A, B, C);
