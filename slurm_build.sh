df22281fa5aaba593ddc2877d5c5437dee1edacb
Daniel <danielelacer08@gmail.com>
2025-08-27 21:13:44 +0100

    Separate build / run benchmark slurm scripts

--- slurm_build.sh
+++ slurm_build.sh
@@ -2,7 +2,7 @@
 #SBATCH -D ./
 #SBATCH --export=ALL
 
-#SBATCH -J BENCHRUNS
+#SBATCH -J BUILD
 
 #SBATCH -o OUTPUT.%j.%a
 #SBATCH -e ERROR.%j.%a
@@ -14,29 +14,41 @@
 
 #SBATCH -t 10
 
-#SBATCH --array=1-7
-BENCHMARKS=(
-    "./bench/add"
-    "./bench/asum"
-    "./bench/mmul"
-    "./bench/mmul_wall"
-    "./bench/scan"
-    "./bench/times_table"
-    "./bench/transpose"
-)
-ARRAY_INDEX=$((SLURM_ARRAY_TASK_ID - 1))
-BENCH_TO_RUN=${BENCHMARKS[$ARRAY_INDEX]}
-
 module load nvhpc
+
 CL_PATH=/opt/apps/pkg/compilers/cuda/12.6.2/targets/x86_64-linux
+
 export LD_LIBRARY_PATH=$CL_PATH/lib:$LD_LIBRARY_PATH
 export LIBRARY_PATH=$CL_PATH/lib:$LIBRARY_PATH
 export C_INCLUDE_PATH=$CL_PATH/include:$C_INCLUDE_PATH
+INC_FLAGS=$(echo "$C_INCLUDE_PATH" | tr ':' ' ')
+
+# library benchs
+make bench/ examples/ EXTRA_INC_DIRS="$INC_FLAGS"
+
+if [ ! -d "CLBlast" ]; then
+    git clone https://github.com/CNugteren/CLBlast
+    cd CLBlast || exit
+    mkdir build install
+    cd build || exit
+    cmake -DCMAKE_INSTALL_PREFIX=../install -DOPENCL_ROOT=$CL_PATH ..
+    make -j"$(nproc)"
+    make install
+    cd ../../
+fi
+
 export LD_LIBRARY_PATH=./CLBlast/install/lib64:$LD_LIBRARY_PATH
 export LIBRARY_PATH=./CLBlast/install/lib64:$LIBRARY_PATH
 export C_INCLUDE_PATH=./CLBlast/install/include:$C_INCLUDE_PATH
+INC_FLAGS=$(echo "$C_INCLUDE_PATH" | tr ':' ' ')
+# CLBlast benchs
+make benchCLBlast/ EXTRA_INC_DIRS="$INC_FLAGS" EXTRA_LD_LIBS=-lclblast
 
-echo Running "$BENCH_TO_RUN" from "$ARRAY_INDEX"
-$BENCH_TO_RUN 1024
+# OpenACC benchs
+for bench in benchOpenACC/*; do
+    nvc -acc -O3 -fast -o "${bench%.c}" "$bench"
+done
+
+./examples/info
 
 # This work made use of the Barkla High Performance Computing facilities at the University of Liverpool.
